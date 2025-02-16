a456acecef975af762c2f2e48d0e8b780583faf1
Muhammad Hamza Shah <110331331+Hamxak462@users.noreply.github.com>
2025-02-17 00:40:58 +0500

    'change h1'

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,24 @@
-import './App.css'
-import { Route , Routes } from 'react-router-dom'
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
 
-import CreateCrud from './Components/CreateCrud'
-import Read from './Components/Read'
-import Edit from './Components/CRUDs/Edit'
-
+import CreateCrud from "./Components/CreateCrud";
+import Read from "./Components/Read";
+import Edit from "./Components/CRUDs/Edit";
 
 function App() {
-
   return (
     <div>
-      <h1>CRUD Operations</h1>
+      <h2>CRUD Operations</h2>
       {/* <CreateCrud /> */}
 
       <Routes>
-        <Route exact path='/' element={<Read/>}></Route>
-        <Route exact path='/create' element={<CreateCrud/>}></Route>
-        <Route path='/edit' element={<Edit />} ></Route>
-        <Route path='*' element={<h2> Path Not found</h2>}></Route>
+        <Route exact path="/" element={<Read />}></Route>
+        <Route exact path="/create" element={<CreateCrud />}></Route>
+        <Route path="/edit" element={<Edit />}></Route>
+        <Route path="*" element={<h2> Path Not found</h2>}></Route>
       </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
